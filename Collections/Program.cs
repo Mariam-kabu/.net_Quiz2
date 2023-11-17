13e8b6b22c970eda7127acbd762e38bad5ceb399
Natia Shpetishvili <102184716+Nshpetishvili@users.noreply.github.com>
2023-11-17 23:44:11 +0400

    Update Program.cs

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -5,5 +5,5 @@ Console.WriteLine("Hello, World!");
 
 Queues.DemonstrateQueues();
 SortedLists.DemonstrateSortedLists();
-BitArray.DemonstrateBitArrays();
+BitArrays.DemonstrateBitArrays();
 Hashtables.DemonstrateHashtable();
