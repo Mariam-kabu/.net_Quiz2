9c3093529e0fa53341a3f711ae4db27fd331e058
Mariam Kabulovi <kabulovimariam@gmail.com>
2023-11-18 09:39:30 +0400

    created an array and  an arraylist demonstrations

--- Collections/obj/Debug/net8.0/Collections.AssemblyInfo.cs
+++ Collections/obj/Debug/net8.0/Collections.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Collections")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de2f0cc5477d3248061e1e54a765e7be91d98c66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Collections")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Collections")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
